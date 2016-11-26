3e3b1fe6732c2e82aa492832e4011d4a253ebfee
edidada <sandisks555@gmail.com>
2016-11-27 01:03:43 +0800

    add python scrip

---
+++ download_src.py
@@ -0,0 +1,28 @@
+import xml.dom.minidom  
+import os  
+from subprocess import call  
+
+rootdir = "G:/android_android-6.0.1-r9_source"  
+
+git = "C:/Program Files/Git/bin/git.exe"  
+      
+dom = xml.dom.minidom.parse("G:/manifest/default.xml")  
+root = dom.documentElement  
+      
+prefix = git + " clone https://aosp.tuna.tsinghua.edu.cn/"  
+suffix = ".git"  
+      
+if not os.path.exists(rootdir):  
+    os.mkdir(rootdir)  
+      
+for node in root.getElementsByTagName("project"):  
+    os.chdir(rootdir)  
+    d = node.getAttribute("path")  
+    last = d.rfind("/")  
+    if last != -1:  
+        d = rootdir + "/" + d[:last]  
+        if not os.path.exists(d):  
+            os.makedirs(d)  
+        os.chdir(d)  
+    cmd = prefix + node.getAttribute("name") + suffix  
+    call(cmd)  
